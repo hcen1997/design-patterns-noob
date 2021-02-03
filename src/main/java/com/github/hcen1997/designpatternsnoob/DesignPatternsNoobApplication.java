b9dcb4c3bdf17eb5651d742db4fe7ee5d8d63343
hcen1997 <panzetaoyx@gamil.com>
2021-02-03 17:10:19 +0800

    init

---
+++ src/main/java/com/github/hcen1997/designpatternsnoob/DesignPatternsNoobApplication.java
@@ -0,0 +1,13 @@
+package com.github.hcen1997.designpatternsnoob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsNoobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsNoobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/hcen1997/designpatternsnoob/DesignPatternsNoobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hcen1997.designpatternsnoob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsNoobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
