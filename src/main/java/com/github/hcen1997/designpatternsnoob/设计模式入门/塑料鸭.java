0019b3cf64402413250f5a91d351c4046d147779
hcen1997 <panzetaoyx@gamil.com>
2021-02-03 17:35:39 +0800

    塑料鸭自定义功能 咯吱叫, 展示塑料材质

--- src/main/java/com/github/hcen1997/designpatternsnoob/设计模式入门/塑料鸭.java
+++ src/main/java/com/github/hcen1997/designpatternsnoob/设计模式入门/塑料鸭.java
@@ -2,4 +2,13 @@ package com.github.hcen1997.designpatternsnoob.设计模式入门;
 
 public class 塑料鸭 extends 鸭子 {
 
+    @Override
+    void 叫() {
+        System.out.println("咯吱咯吱");
+    }
+
+    @Override
+    void 展示() {
+        System.out.println("我是一只塑料做的鸭子");
+    }
 }
